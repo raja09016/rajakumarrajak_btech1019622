66401df4eb8568459015ee0cdbce25dfb36df100
raja09016 <rajarajak2121raja@gmail.com>
2026-01-13 20:44:45 +0530

    final

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ connectDB();
 
 // Middleware
 app.use(cors({
-  origin: process.env.CLIENT_URL || 'http://localhost:5173' || 'https://rajakumarrajak-btech1019622.vercel.app',
+  origin: process.env.CLIENT_URL || 'http://localhost:5173' ,
   
   credentials: true
 }));
